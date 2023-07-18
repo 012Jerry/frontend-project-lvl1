76226fdf1477cc739df53054f9e367919a3f0669
012Jerry <555519@list.ru>
2023-07-18 16:47:40 +0500

    step 5

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import readlineSync from 'readline-sync';
 
-//const readlineSync = require('readline-sync');
-
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
+  min = Math.ceil(min); 
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
+  
 function game() { 
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
+  const result = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${result}!\nWhat is the result of the expression?`);
   for (let i = 0; i <= 2; i += 1) {
     const num = getRandomIntInclusive(1, 15)
     console.log(`${'Question:'}${num}`);
@@ -19,10 +17,10 @@ function game() {
     if (num % 2 === 0 && answer === 'yes' || num % 2 !== 0 && answer === 'no' ) {
       console.log('Correct!');
     } else {
-      console.log(`${"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, "}${name}${'!'}`);
+      console.log(`${"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, "}${result}${'!'}`);
       return;
     }
-  }
-  console.log(`${'Congratulations '}${name}${'!'}`);
+}
+console.log(`${'Congratulations '}${result}${'!'}`);
 }
 export { game };
